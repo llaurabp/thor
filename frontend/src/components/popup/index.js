cfb9c03f84601927a02326d910601271cf2fa861
Samuel F <samuelf@unifor.br>
2021-11-22 11:37:15 -0300

    removing window.location.reload

--- frontend/src/components/popup/index.js
+++ frontend/src/components/popup/index.js
@@ -1,72 +1,95 @@
 import React, { useState, useEffect } from "react";
-import './styles.css';
-import api from '../../services/api.js';
+import "./styles.css";
+import api from "../../services/api.js";
 
 function initialState() {
-    return {
-    marca: "", 
-    valor: 0, 
+  return {
+    marca: "",
+    valor: 0,
     data: new Date(),
     quantidade: 0,
-    } }
+  };
+}
 
 const Modal = (props) => {
-    const [values, setValues] = useState(initialState);
+  const [values, setValues] = useState(initialState);
 
-    const token = localStorage.getItem('@Zeus:token')
-// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjQ0Y2Y4MDFmMTI3NDA1OWU2OGI0YSIsImlhdCI6MTYzNDI5MzY1MSwiZXhwIjoxNjM0MzgwMDUxfQ.SVRr04qmPCf4yykuYQdeHhXsHKOwEpigeEmKUwG8ta8";
+  const token = localStorage.getItem("@Zeus:token");
+  // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjQ0Y2Y4MDFmMTI3NDA1OWU2OGI0YSIsImlhdCI6MTYzNDI5MzY1MSwiZXhwIjoxNjM0MzgwMDUxfQ.SVRr04qmPCf4yykuYQdeHhXsHKOwEpigeEmKUwG8ta8";
 
-    useEffect (() => {
-        console.log(values)
-    },[values])
+  useEffect(() => {
+    console.log(values);
+  }, [values]);
 
+  if (!props.show) {
+    return null;
+  }
 
-    if (!props.show) { 
-        return null
-    }
-
-const sendTransaction = async (e) => {
+  const sendTransaction = async (e) => {
     e.preventDefault();
-const data = values;
-   
-try {
-    await api.post("/secondpage/create", data, {
+    const data = values;
+
+    try {
+      await api.post("/secondpage/create", data, {
         headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });  
+          Authorization: `Bearer ${token}`,
+        },
+      });
     } catch (err) {
-        console.log(err);
-  
+      console.log(err);
     }
-    window.location.reload();
-}
+    try {
+      props.getList();
+      props.setShow(false);
+    } catch (err) {}
+  };
 
-
-  
-    return (
-        
-        <div className="modal">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h4 className="modal-title">Nova</h4>
-                </div>
-                <form className="modal-body" onSubmit={sendTransaction}>
-              
-                    <input type="text" placeholder="Marca da ração" onChange={(e) => {setValues({...values, marca:e.target.value})}}/>
-
-                    <input type="number" placeholder="Valor" onChange={(e) => {setValues({...values, valor:e.target.value})}}/>
-
-                    <input type="number" placeholder="Quantidade de ração" onChange={(e) => {setValues({...values, quantidade:e.target.value})}}/>
-                 
-                    <input type="date" onChange={(e) => {setValues({...values, data:new Date(e.target.value)})}}/>
-                <button type="submit" className="button" >Adicionar</button>
-                <button onClick={props.onClose}className="button">Fechar</button>
-                </form>
-              
-            </div>
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h4 className="modal-title">Nova</h4>
         </div>
-      
-     )
-}
+        <form className="modal-body" onSubmit={sendTransaction}>
+          <input
+            type="text"
+            placeholder="Marca da ração"
+            onChange={(e) => {
+              setValues({ ...values, marca: e.target.value });
+            }}
+          />
+
+          <input
+            type="number"
+            placeholder="Valor"
+            onChange={(e) => {
+              setValues({ ...values, valor: e.target.value });
+            }}
+          />
+
+          <input
+            type="number"
+            placeholder="Quantidade de ração"
+            onChange={(e) => {
+              setValues({ ...values, quantidade: e.target.value });
+            }}
+          />
+
+          <input
+            type="date"
+            onChange={(e) => {
+              setValues({ ...values, data: new Date(e.target.value) });
+            }}
+          />
+          <button type="submit" className="button">
+            Adicionar
+          </button>
+          <button onClick={props.onClose} className="button">
+            Fechar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Modal;
--- frontend/src/pages/SecondPage/index.js
+++ frontend/src/pages/SecondPage/index.js
@@ -1,71 +1,83 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // import {FiLogIn} from 'react-icons/fi';
-import { MdDarkMode, MdModeEditOutline } from 'react-icons/md';
-import { IoMdArrowBack, IoMdRemoveCircleOutline, } from 'react-icons/io';
-import './styles.css';
-import '../../global.css';
-import logoImg from '../../assets/Frame 1.svg';
-import logoImg2 from '../../assets/Frame 1 (1).svg';
+import { MdDarkMode, MdModeEditOutline } from "react-icons/md";
+import { IoMdArrowBack, IoMdRemoveCircleOutline } from "react-icons/io";
+import "./styles.css";
+import "../../global.css";
+import logoImg from "../../assets/Frame 1.svg";
+import logoImg2 from "../../assets/Frame 1 (1).svg";
 import Modal from "../../components/popup";
 import EditModal from "../../components/editPopup";
 
 import api from "../../services/api";
-const token = localStorage.getItem('@Zeus:token');
+const token = localStorage.getItem("@Zeus:token");
 
 // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjQ0Y2Y4MDFmMTI3NDA1OWU2OGI0YSIsImlhdCI6MTYzNDI5MzY1MSwiZXhwIjoxNjM0MzgwMDUxfQ.SVRr04qmPCf4yykuYQdeHhXsHKOwEpigeEmKUwG8ta8";
 export default function SecondPage() {
-  const [show, setShow] = useState(false)
-  const [showw, setShoww] = useState(false)
-  const [purchases, setPurchases] = useState([])
+  const [show, setShow] = useState(false);
+  const [showw, setShoww] = useState(false);
+  const [purchases, setPurchases] = useState([]);
   const [image, setImage] = useState(logoImg);
   const [status, setStatus] = useState([]);
   const [id, setId] = useState("");
   const [totalV, setTotalV] = useState();
   const [totalQ, setTotalQ] = useState();
 
-  useEffect(() => {
-    api.get("/secondpage/get_many", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+  const getList = async () => {
+    await api
+      .get("/secondpage/get_many", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
       .then((res) => {
         // console.log(res.data)
-        setPurchases(res.data)
-        setTotalV(res.data.map(item => item.valor).reduce((total, item) => total + item), 0)
-        setTotalQ(res.data.map(item => item.quantidade).reduce((total, item) => total + item), 0)
+        setPurchases(res.data);
+        setTotalV(
+          res.data
+            .map((item) => item.valor)
+            .reduce((total, item) => total + item, 0)
+        );
+        setTotalQ(
+          res.data
+            .map((item) => item.quantidade)
+            .reduce((total, item) => total + item, 0)
+        );
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [])
-
+  };
+  useEffect(() => {
+    getList();
+  }, []);
 
   const deleteItem = (id) => {
-    api.delete(`/secondpage/delete/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    window.location.reload();
-  }
-
+    api
+      .delete(`/secondpage/delete/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => {
+        getList();
+      });
+  };
 
   function darkmode() {
     var someElement = document.getElementById("root");
     if (someElement.className == "light") {
       someElement.className = "";
       someElement.className += "dark";
-      setImage(logoImg2)
+      setImage(logoImg2);
     } else if (someElement.className == "dark") {
       someElement.className = "light";
-      setImage(logoImg)
+      setImage(logoImg);
     }
   }
 
   return (
-
     <div className="teste">
       <header>
         <img src={image} className="new-logo" id="logotest" alt="Logo" />
@@ -83,33 +95,41 @@ export default function SecondPage() {
         </div>
 
         <div className="left">
-
-
           <div className="modaltest">
-            <button className="transaction-button" onClick={() => setShow(true)} onclick="showModal()">Nova transação</button>
-            <Modal onClose={() => setShow(false)} show={show} />
+            <button
+              className="transaction-button"
+              onClick={() => setShow(true)}
+              onclick="showModal()"
+            >
+              Nova transação
+            </button>
+            <Modal
+              onClose={() => setShow(false)}
+              show={show}
+              setShow={setShow}
+              getList={getList}
+            />
           </div>
 
           <div className="backk">
-
             <Link to="/">
-              <button className="back-button" >
-                <IoMdArrowBack size={16} color="#fff" /></button>
+              <button className="back-button">
+                <IoMdArrowBack size={16} color="#fff" />
+              </button>
             </Link>
           </div>
 
-
           <div className="darkk">
-            <button className="darkmode-button" onClick={darkmode}> <MdDarkMode size={16} color="#fff" /></button>
+            <button className="darkmode-button" onClick={darkmode}>
+              {" "}
+              <MdDarkMode size={16} color="#fff" />
+            </button>
           </div>
 
-
           <section id="transactions">
-
             <h2 class="sr-only">Transações</h2>
 
             <table id="data-table">
-
               <tr>
                 <th>Marca</th>
                 <th>Qtd de ração</th>
@@ -123,27 +143,37 @@ export default function SecondPage() {
                     <td>{purchase.marca}</td>
                     <td>{purchase.quantidade}</td>
                     <td>{purchase.valor}</td>
-                    <td>{`${purchase.data.slice(5, 7)}/${purchase.data.slice(8, 10)}/${purchase.data.slice(0, 4)}`}</td>
+                    <td>{`${purchase.data.slice(5, 7)}/${purchase.data.slice(
+                      8,
+                      10
+                    )}/${purchase.data.slice(0, 4)}`}</td>
                     <td id="remove">
-                      <button onClick={() => deleteItem(purchase._id)} >
+                      <button onClick={() => deleteItem(purchase._id)}>
                         <IoMdRemoveCircleOutline />
                       </button>
-                      <button onClick={() => {
-                        setId(purchase._id)
-                        setShoww(true);
-                      }}   >
+                      <button
+                        onClick={() => {
+                          setId(purchase._id);
+                          setShoww(true);
+                        }}
+                      >
                         <MdModeEditOutline />
                       </button>
                     </td>
                   </tr>
-                )
+                );
               })}
-
             </table>
           </section>
-
-        </div></div>
-      <EditModal onClose={() => setShoww(false)} showw={showw} id={id} />
+        </div>
+      </div>
+      <EditModal
+        onClose={() => setShoww(false)}
+        showw={showw}
+        id={id}
+        setShoww={setShoww}
+        getList={getList}
+      />
     </div>
   );
 }
